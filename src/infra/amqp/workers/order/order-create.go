7bc3484fd56b974a8d655fff779d908a15608c13
Joao Henrique Duarte <joao.duarte@maxmilhas.com.br>
2022-11-14 17:17:51 -0300

    refactor: Centralize CountProcessedMessage function

--- src/infra/amqp/workers/order/order-create.go
+++ src/infra/amqp/workers/order/order-create.go
@@ -1,12 +1,9 @@
 package order
 
 import (
-	"fmt"
 	"log"
 	"rabbitmq-golang/src/infra/amqp"
 	"rabbitmq-golang/src/infra/amqp/workers"
-	"strconv"
-	"strings"
 
 	"github.com/rabbitmq/amqp091-go"
 )
@@ -25,9 +22,10 @@ func (o *OrderCreateWorker) Start(queueName string, maxRetriesConfig int) error 
 
 	for message := range out {
 		log.Print("[OrderCreateWorker::Consume] Message Received")
+
 		err := o.HandleMessage.Handle(message)
 		if err != nil {
-			if CountProcessedMessage(message) >= maxRetriesConfig {
+			if workers.CountProcessedMessage(message) >= maxRetriesConfig {
 				log.Printf(
 					"[OrderCreateWorker::Consume] This order exceeded %d processing attempts",
 					maxRetriesConfig,
@@ -41,25 +39,4 @@ func (o *OrderCreateWorker) Start(queueName string, maxRetriesConfig int) error 
 		}
 	}
 	return nil
-}
-
-func CountProcessedMessage(message amqp091.Delivery) int {
-	header := message.Headers["x-death"]
-
-	result := (fmt.Sprint(header))
-
-	entries := strings.Split(result, "map[count:")
-
-	countProccess := 0
-
-	for key, value := range entries {
-		if key == 1 {
-			count, err := strconv.Atoi(strings.Split(value, " ")[0])
-			if err != nil {
-				log.Printf("[CountProcessedMessage] Error in get message counter processing: %s", err)
-			}
-			countProccess = count
-		}
-	}
-	return countProccess
 }
--- src/infra/amqp/workers/workers.go
+++ src/infra/amqp/workers/workers.go
@@ -1,5 +1,34 @@
 package workers
 
+import (
+	"encoding/json"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/rabbitmq/amqp091-go"
+)
+
 type Worker interface {
 	Start(queueName string)
+}
+
+func CountProcessedMessage(message amqp091.Delivery) int {
+	header, err := json.Marshal(message.Headers["x-death"])
+	if err != nil {
+		log.Printf("[CountProcessedMessage] Error in convert header message: %s", err)
+	}
+
+	entries := strings.Split(strings.Trim(string(header), "[{}]"), ",")
+
+	countProccess := 0
+
+	for _, value := range entries {
+		entrie := strings.Split(value, ":")
+		if entrie[0] == `"count"` {
+			count, _ := strconv.Atoi(entrie[1])
+			countProccess = count
+		}
+	}
+	return countProccess
 }
