98886996079c9f1c52083bf19ae2f90089bb9e81
JoaoDuarte10 <joaoduarte7341@gmail.com>
2022-11-14 23:02:21 -0300

    refactor: Improves text logger

--- src/factories/database.go
+++ src/factories/database.go
@@ -8,7 +8,7 @@ import (
 func MakeConnectionDatabse() sql.DB {
 	db, err := sql.Open("sqlite3", "./order.db")
 	if err != nil {
-		log.Fatalf("Error in connect to database: %s", err)
+		log.Fatalf("[MakeConnectionDatabse] Error in connect to database: %s", err)
 	}
 	return *db
 }
